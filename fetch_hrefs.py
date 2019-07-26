1ce9ebaceda4569239c519132abf49ab9afed0d3
jinfull <jinfull@gmail.com>
2019-07-25 22:23:54 -0700

    Add working inifinite URI scraping

--- fetch_hrefs.py
+++ fetch_hrefs.py
@@ -1,14 +1,18 @@
-import requests, bs4
+import requests
+import bs4
 
 # fetch_hrefs('http://jinfull.com') => ['http://github.com/jinfull', 'http://asano.herokuapp.com', ...]
 def fetch_hrefs(link):
   links_arr = []
-
-  link_res = requests.get(link)
-  link_res_soup = bs4.BeautifulSoup(link_res.text, 'html.parser')
+  try: 
+    link_res = requests.get(link)
+    link_res_soup = bs4.BeautifulSoup(link_res.text, 'html.parser')
+  except: 
+    return []
 
   for link in link_res_soup.find_all('a'):
     curr_link = str(link.get('href'))
+    # curr_link = link.get('href').encode('utf-8')
 
     if (curr_link == 'None'):
       continue
@@ -16,6 +20,6 @@ def fetch_hrefs(link):
     if (curr_link[0:4] == 'http'):
       links_arr.append(curr_link)
 
-  print links_arr
+  print (links_arr)
 
-fetch_hrefs('http://rescale.com')
+fetch_hrefs('https://www.facebook.com/rescaleinc/')
--- webcrawler.py
+++ webcrawler.py
@@ -1,33 +1,49 @@
-import requests, bs4
-
+# import requests, bs4
 from fetch_hrefs import fetch_hrefs
 
 
 root = 'https://www.rescale.com/'
-root_res = requests.get(root)
-root_res_soup = bs4.BeautifulSoup(root_res.text, 'html.parser')
-
-# print root
-# print root_res
-# print root_res_soup
 
 links_queue = [root]
+links_set = set()
 
 while (links_queue):
-  # print links_queue.pop(0)
+  # pop off the first item in links_queue and assign to curr_link
   curr_link = links_queue.pop(0)
   
+  print (curr_link)
+
+  # get all links of the curr_link and assign (array) to curr_links
+  curr_links = fetch_hrefs(curr_link)
+
+  print (curr_links)
+
+  # iterate through of all the current link's links
+  # if they are already in our 'seen', we continue
+  # else add them to the end of our links_queue
+  for link in curr_links:
+    if link in links_set:
+      continue
+    else:
+      links_queue.append(link)
+      links_set.add(link)
+    
+
+  # print curr_links
+  
+
+# ==========================================
+# ==========================================
+
+# IGNORE ME
 
 
-
-
-fetch_hrefs('http://rescale.com')
+# fetch_hrefs('http://rescale.com')
 # fetch_hrefs('http://jinfull.com') => ['http://github.com/jinfull', 'http://asano.herokuapp.com', ...]
 
 
 
 
-# ==========================================
 # ==========================================
 
 
