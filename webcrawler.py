a93d4965438ad3782fe9a457fe71256eac87e22f
jinfull <jinfull@gmail.com>
2019-07-30 13:04:20 -0700

    Add synchronous queue implementation to continuously crawl

--- webcrawler.py
+++ webcrawler.py
@@ -5,21 +5,47 @@ from fetch_hrefs import fetch_hrefs
 from print_link_block import print_link_block
 
 root = sys.argv[1]
-root_links = fetch_hrefs(root)
+root_links = [] + fetch_hrefs(root)
 
-def webcrawler(link): 
-  curr_links = fetch_hrefs(link)
-  print_link_block(link, curr_links)
-  
-  # print('task executed by {}'.format(threading.current_thread()))
-
+queue = []
+seen = set()
 
 def main():
   print_link_block(root, root_links)
 
-  with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-    for link in root_links:
-      executor.submit(webcrawler, link)
+  while root_links:
+    curr_link = root_links.pop(0)
+    curr_links = fetch_hrefs(curr_link)
+
+    for link in curr_links:
+      if link in seen:
+        continue
+      else:
+        root_links.append(link)
+        seen.add(link)
+
+    print_link_block(curr_link, curr_links)
+
+
+def webcrawler(link): 
+  curr_links = fetch_hrefs(link)
+  print_link_block(link, curr_links)
+
+  # root_links += curr_links
+  # links_set.add(root_links.pop(0))
+
+  # print('task executed by {}'.format(threading.current_thread()))
+
+
+# def main():
+#   print_link_block(root, root_links)
+
+#   with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+#     # while len(root_links) > 0:
+#       # executor.submit(webcrawler, root_links[0])
+
+#     for link in root_links:
+#       executor.submit(webcrawler, link)
 
 
 if __name__ == '__main__':
