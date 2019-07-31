400e5b63f4272ac47fa5031b608901d2d98d3d9e
jinfull <jinfull@gmail.com>
2019-07-31 00:33:14 -0700

    Queue implementation to continuously crawl

--- webcrawler.py
+++ webcrawler.py
@@ -5,46 +5,58 @@ from fetch_hrefs import fetch_hrefs
 from print_link_block import print_link_block
 
 root = sys.argv[1]
-root_links = [] + fetch_hrefs(root)
+queue = fetch_hrefs(root)
 
-queue = []
 seen = set()
 
 def main():
-  print_link_block(root, root_links)
-
-  while root_links:
-    curr_link = root_links.pop(0)
-    curr_links = fetch_hrefs(curr_link)
-
-    for link in curr_links:
-      if link in seen:
-        continue
-      else:
-        root_links.append(link)
-        seen.add(link)
-
-    print_link_block(curr_link, curr_links)
+  print_link_block(root, queue)
 
 
-def webcrawler(link): 
-  curr_links = fetch_hrefs(link)
-  print_link_block(link, curr_links)
+  with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    while queue:
+      # curr_link = queue.pop(0)
+      executor.submit(webcrawler, queue)
+      # print (queue)
+    # curr_links = fetch_hrefs(curr_link)
 
-  # root_links += curr_links
-  # links_set.add(root_links.pop(0))
 
-  # print('task executed by {}'.format(threading.current_thread()))
+    # print_link_block(curr_link, curr_links)
+  print('-----------------------------------------------------------------------------------------')
+  print (queue)
+  print('-----------------------------------------------------------------------------------------')
+
+
+
+def webcrawler(links): 
+  # print('-----------------------------------------------------------------------------------------')
+  # print (queue)
+  # print('-----------------------------------------------------------------------------------------')
+
+  curr_link = queue.pop(0)
+  curr_links = fetch_hrefs(curr_link)
+
+  print_link_block(curr_link, curr_links)
+
+  for fetched_link in curr_links:
+    if fetched_link in seen:
+      break
+    else:
+      seen.add(fetched_link)
+      queue.append(fetched_link)
+
+  # queue += curr_links
+  # links_set.add(queue.pop(0))
+
+  print('task executed by {}'.format(threading.get_ident()))
 
 
 # def main():
-#   print_link_block(root, root_links)
+#   print_link_block(root, queue)
 
 #   with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-#     # while len(root_links) > 0:
-#       # executor.submit(webcrawler, root_links[0])
 
-#     for link in root_links:
+#     for link in queue:
 #       executor.submit(webcrawler, link)
 
 
